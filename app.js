b82336f57ceaea2a96a4d78d8b1583af11c90b7e
vinchiengithub <jianfan0114@gmail.com>
2023-04-29 20:06:07 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ if (process.env.NODE_ENV !== 'production') {
 }
 // 載入 method-override
 const methodOverride = require('method-override');
+// 引用 connect-flash
+const flash = require('connect-flash');
 // 引用 body-parser
 const bodyParser = require('body-parser');
 // 引用路由器
@@ -47,11 +49,15 @@ app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app);
+// 掛載 connect-flash
+app.use(flash());
 // 在 usePassport(app) 之後、app.use(routes) 之前，加入一組 middleware
 app.use((req, res, next) => {
   // 你可以在這裡 console.log(req.user) 等資訊來觀察
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash('success_msg'); // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg'); // 設定 warning_msg 訊息
   next();
 });
 // 將 request 導入路由器
