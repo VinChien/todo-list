8a4906cda5454d83d7b4478e5dbb69eb971c29aa
vinchiengithub <jianfan0114@gmail.com>
2023-04-29 17:16:17 +0800

    feat: add user login feature via passport

--- config/passport.js
+++ config/passport.js
@@ -29,4 +29,13 @@ module.exports = (app) => {
     })
   );
   // 設定序列化與反序列化
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .lean()
+      .then((user) => done(null, user))
+      .catch((err) => done(err, null));
+  });
 };
