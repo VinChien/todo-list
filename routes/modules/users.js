a785638971a4bc787bb4b137df10ac5c974c1998
vinchiengithub <jianfan0114@gmail.com>
2023-04-29 20:49:31 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const User = require('../../models/user');
 // 引用 passport
 const passport = require('passport');
+// 引用 bcrypt
+const bcrypt = require('bcryptjs');
+const { config } = require('dotenv');
 
 router.get('/login', (req, res) => {
   res.render('login');
@@ -42,30 +45,36 @@ router.post('/register', (req, res) => {
     });
   }
   // 檢查使用者是否已經註冊
-  User.findOne({ email })
-    .then((user) => {
-      // 如果已經註冊：退回原本畫面
-      if (user) {
-        errors.push({ message: '這個 Email 已經註冊過了。' });
-        res.render('register', {
-          errors,
-          name,
-          email,
-          password,
-          confirmPassword,
-        });
-      } else {
-        // 如果還沒註冊：寫入資料庫
-        return User.create({
-          name,
-          email,
-          password,
-        })
-          .then(() => res.redirect('/'))
-          .catch((err) => console.log(err));
-      }
-    })
-    .catch((err) => console.log(err));
+  User.findOne({ email }).then((user) => {
+    // 如果已經註冊：退回原本畫面
+    if (user) {
+      errors.push({ message: '這個 Email 已經註冊過了。' });
+      res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword,
+      });
+    }
+    return (
+      bcrypt
+        // 產生「鹽」，並設定複雜度係數為 10
+        .genSalt(10)
+        // 為使用者密碼「加鹽」，產生雜湊值
+        .then((salt) => bcrypt.hash(password, salt))
+        .then((hash) =>
+          User.create({
+            name,
+            email,
+            // 用雜湊值取代原本的使用者密碼
+            password: hash,
+          })
+        )
+        .then(() => res.redirect('/'))
+        .catch((err) => console.log(err))
+    );
+  });
 });
 
 router.get('/register', (req, res) => {
