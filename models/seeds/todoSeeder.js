5f9f6dfaa17d1895856618add98a6b58fd850656
vinchiengithub <jianfan0114@gmail.com>
2023-04-30 19:08:14 +0800

    modify todoSeeder

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,9 +1,23 @@
+const bcrypt = require('bcryptjs');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
 // const mongoose = require('mongoose');
 // 載入 todo model
 const Todo = require('../todo');
 
+const User = require('../user');
+
 // 引用 mongoose 設定檔
 const db = require('../../config/mongoose');
+
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678',
+};
 
 // if (process.env.NODE_ENV !== 'production') {
 //   require('dotenv').config();
@@ -20,12 +34,39 @@ const db = require('../../config/mongoose');
 //   console.log('mongodb error!');
 // });
 
+// db.once('open', () => {
+//   console.log('mongodb connected!');
+
+//   for (let i = 0; i < 10; i++) {
+//     Todo.create({ name: `name-${i}` });
+//   }
+
+//   console.log('done');
+// });
 db.once('open', () => {
-  console.log('mongodb connected!');
-
-  for (let i = 0; i < 10; i++) {
-    Todo.create({ name: `name-${i}` });
-  }
-
-  console.log('done');
+  bcrypt
+    .genSalt(10)
+    .then((salt) => bcrypt.hash(SEED_USER.password, salt))
+    .then((hash) =>
+      User.create({
+        name: SEED_USER.name,
+        email: SEED_USER.email,
+        password: hash,
+      })
+    )
+    .then((user) => {
+      const userId = user._id;
+      // for (let i = 0; i < 10; i++) {
+      //   Todo.create({ name: `name-${i}`, userId });
+      // }
+      return Promise.all(
+        Array.from({ length: 10 }, (_, i) =>
+          Todo.create({ name: `name-${i}`, userId })
+        )
+      );
+    })
+    .then(() => {
+      console.log('done.');
+      process.exit();
+    });
 });
